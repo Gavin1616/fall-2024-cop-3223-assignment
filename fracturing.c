eadbee0584a3ab8688e836783f4900468f0ede92
Gavin1616 <166345284+Gavin1616@users.noreply.github.com>
2024-09-07 04:20:13 +0000

    new

--- fracturing.c
+++ fracturing.c
@@ -24,16 +24,24 @@ double calculateDistance()
 
         return distance;
     }
+    distance_between_each_point()
+    {
+       
+    }
 double calculatePerimeter()
 {
-    double perimeter_side = 0;
-    double perimeter_total = 0;
-    perimeter_side = calculateDistance();
-    perimeter_total = perimeter_side * 4;
-    printf("The Perimeter is: %.2f\n", perimeter_total);
+     int x1;
+        int y1;
+        int x2;
+        int y2;
+        scanf("%d %d", &x1, &y1);
+        scanf("%d %d", &x2, &y2);
+
+        printf("Point #1 entered: x1 = %d; y1 = %d\n",x1, y1);
+        printf("Point #2 entered: x2= %d: y2= %d\n", x2, y2);
+
 }
 int main(int argc, char **argv)
 {
     calculateDistance();
-    calculatePerimeter();
 }
---
+++ new.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+double calculateDistance()
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        double distance = 0;
+        
+        scanf("%d %d", &x1, &y1);
+        scanf("%d %d", &x2, &y2);
+
+        printf("Point #1 entered: x1 = %d; y1 = %d\n",x1, y1);
+        printf("Point #2 entered: x2= %d: y2= %d\n", x2, y2);
+
+        distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        printf("The distance between the two points is %.2f\n", distance);
+
+        return distance;
+    }
+
+distance_between_x(int x1, int x2)
+{
+       return fabs((double)(x2 - x1);
+}
+distance_between_y(int y1, int y2)
+{
+    return fabs((double)(y2 - y1);
+}
+
+double calculatePerimeter()
+{
+    double x_distance = 0;
+    double y_distance = 0;
+    double perimeter = 0;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    scanf("%d %d", &x1, &y1);
+    scanf("%d %d", &x2, &y2);
+
+    printf("Point #1 entered: x1 = %d; y1 = %d\n",x1, y1);
+    printf("Point #2 entered: x2= %d: y2= %d\n", x2, y2);
+
+    x_distance = distance_between_x(x1, x2);   
+    y_distance = distance_between_y(y1, y2);
+
+    perimeter = 2 * x_distance + 2 * y_distance;
+
+    printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
+
+}
+int main(int argc, char **argv)
+{
+    calculateDistance();
+    calculatePerimeter();
+}
