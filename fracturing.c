c2e41ce60ca4135ca43056b843cedf4c25acee49
Gavin1616 <166345284+Gavin1616@users.noreply.github.com>
2024-09-07 02:26:16 +0000

    Updated work

---
+++ fracturing.c
@@ -0,0 +1,38 @@
+//********************************************************
+// fracturing.c
+// Author: Gavin Zieglar
+// Class: COP 3223, Professor Parra
+// //********************************************************
+#include <stdio.h>
+#include <math.h>
+double calculateDistance()
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        double distance = 0;
+
+        scanf("%d %d", &x1, &y1);
+        printf("Point #1 entered: x1 = %d; y1 = %d",x1, y1);
+        scanf("%d %d", &x2, &y2);
+        printf("Point #2 entered: x2= %d: y2= %d", x2, y2);
+
+        distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        return distance;
+    }
+double calculatePerimeter()
+{
+    double perimeter_side = 0;
+    double perimeter_total = 0;
+    perimeter_side = calculateDistance();
+    peremeter_total = perimeter_side * 4;
+    printf("The Perimeter is: %.2f\n", peremeter_total);
+}
+int main(int argc, char **argv)
+{
+    calculatePerimeter();
+    calculateDistance();
+
+}
