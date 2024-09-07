2c2dcbd5e4b09fb3a0d446ff0e037c3751db2b1b
Gavin1616 <166345284+Gavin1616@users.noreply.github.com>
2024-09-07 04:35:58 +0000

    more

--- new.c
+++ new.c
@@ -20,13 +20,13 @@ double calculateDistance()
         return distance;
     }
 
-distance_between_x(int x1, int x2)
+double distance_between_x(int x1, int x2)
 {
-       return fabs((double)(x2 - x1);
+       return fabs((double)(x2 - x1));
 }
-distance_between_y(int y1, int y2)
+double distance_between_y(int y1, int y2)
 {
-    return fabs((double)(y2 - y1);
+    return fabs((double)(y2 - y1));
 }
 
 double calculatePerimeter()
@@ -53,8 +53,34 @@ double calculatePerimeter()
     printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
 
 }
+double calculateArea()
+{
+    double area = 0;
+    double variable_x = 0;
+    double variable_y = 0;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    scanf("%d %d", &x1, &y1);
+    scanf("%d %d", &x2, &y2);
+
+    printf("Point #1 entered: x1 = %d; y1 = %d\n",x1, y1);
+    printf("Point #2 entered: x2= %d: y2= %d\n", x2, y2);
+
+
+    variable_x = distance_between_x(x1, x2);   
+    variable_y = distance_between_y(y1, y2);
+    area = variable_x * variable_y;
+    printf("The area of the city encompassed by your request is %.2f\n", area);
+
+    
+}
 int main(int argc, char **argv)
 {
     calculateDistance();
     calculatePerimeter();
+    calculateArea();
 }
